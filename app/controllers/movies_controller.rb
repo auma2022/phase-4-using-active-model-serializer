f6cf59530e3a58313a6f3ea4d7d8182be3509368
auma2022 <dianaauma2@gmail.com>
2022-10-03 11:52:12 +0300

    commit tree ce41bcd1cbfff499eacb7aeae39856e22f6a854b

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  attributes :id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director
 
   def index
     movies = Movie.all
@@ -8,9 +9,8 @@ class MoviesController < ApplicationController
 
   def show
     movie = Movie.find(params[:id])
-    render json: movie
+    render json: movie.to_json(only: [:id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director])
   end
-
   private
 
   def render_not_found_response
